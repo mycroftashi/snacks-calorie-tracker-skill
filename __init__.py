9f46ff5620745e9421a05f497ddd68fd9dd1c985
mycroftashi <73615955+mycroftashi@users.noreply.github.com>
2020-11-02 11:26:15 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import Read_Write_Calories.py
 
 class SnacksCalorieTracker(MycroftSkill):
     def __init__(self):
@@ -8,7 +8,7 @@ class SnacksCalorieTracker(MycroftSkill):
     @intent_file_handler('tracker.calorie.snacks.intent')
     def handle_tracker_calorie_snacks(self, message):
         self.speak_dialog('tracker.calorie.snacks')
-
+        execfile('Read_Write_Calories.py')
 
 
 def create_skill():
