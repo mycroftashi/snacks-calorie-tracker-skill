6e147e3f085c3a303130afd008d41e7c2ccfc8e0
mycroftashi <73615955+mycroftashi@users.noreply.github.com>
2020-11-02 18:18:20 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,22 +1,22 @@
 from adapt.intent import IntentBuilder
-from mycroft import MycroftSkill, intent_file_handler, intent_handler
-from mycroft.skills.context import adds_context, removes_context
-import os.path
-import json
+from mycroft import MycroftSkill, intent_handler
 
-class SnacksCalorieTracker(MycroftSkill):
-    def __init__(self):
-
-        MycroftSkill.__init__(self)
+class PotatoSkill(MycroftSkill):
 
     @intent_handler(IntentBuilder('WhatIsPotato').require('What')
                     .require('Potato'))
     def handle_what_is(self, message):
         self.speak_dialog('potato.description')
 
+    @intent_handler(IntentBuilder('DoYouLikePotato').require('Potato')
+                    .require('Like').optionally('Type').one_of('You', 'I'))
+    def handle_do_you_like(self, message):
+        potato_type = message.data.get('Type')
+        if potato_type is not None:
+            self.speak_dialog('like.potato.type',
+                              {'type': potato_type})
+        else:
+            self.speak_dialog('like.potato.generic')
 
-    def stop(self):
-        pass
 def create_skill():
-    return SnacksCalorieTracker()
-
+    return PotatoSkill()
