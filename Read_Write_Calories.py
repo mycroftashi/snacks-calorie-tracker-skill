3a80baf8123a63611149d131a8729803ebb5ba3a
mycroftashi <73615955+mycroftashi@users.noreply.github.com>
2020-11-02 11:03:28 -0500

    Update Read_Write_Calories.py

--- Read_Write_Calories.py
+++ Read_Write_Calories.py
@@ -5,3 +5,25 @@ with open('test/Calorie_Master.json') as f:
 
 # Output: {'name': 'Bob', 'languages': ['English', 'Fench']}
 print(data)
+
+
+def write_json(data, filename='DailySnackTracker.json'):
+	with open(filename, 'w') as f:
+		json.dump(data, f, indent=4)
+
+
+with open('DailySnackTracker.json') as json_file:
+	data = json.load(json_file)
+
+	temp = data['Snacks']
+
+	# python object to be appended
+	y = {"snack": 'Cheerios',
+	     "quantity": "1",
+	     "consumed": "900"
+	     }
+
+	# appending data to emp_details
+	temp.append(y)
+
+write_json(data)
