c5f98e677905a9befc2de6aaa11eff6f1a137ab5
mycroftashi <73615955+mycroftashi@users.noreply.github.com>
2020-11-02 12:53:09 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
+import os.path
 import json
 
 class SnacksCalorieTracker(MycroftSkill):
@@ -8,13 +9,26 @@ class SnacksCalorieTracker(MycroftSkill):
     @intent_file_handler('tracker.calorie.snacks.intent')
     def handle_tracker_calorie_snacks(self, message):
         self.speak_dialog('tracker.calorie.snacks')
-        with open('test/Calorie_Master.json') as f:
-            data = json.load(f)
+        tracker = os.path.expanduser("~/test/DailySnackTracker.json")
+       # filename = os.path.expanduser("~/test/Calorie_Master.json")
+       # with open(filename) as f:
+        #    data = json.load(f)
 
         # Output: {'name': 'Bob', 'languages': ['English', 'Fench']}
-        print(data)
+        #print(data)
+        with open(tracker) as json_file:
+            data = json.load(json_file)
 
+        temp = data['Snacks']
 
+        # python object to be appended
+        y = {"snack": 'Cheery',
+             "quantity": "1",
+             "consumed": "900"
+             }
+
+        # appending data to emp_details
+        temp.append(y)
 
 def create_skill():
     return SnacksCalorieTracker()
