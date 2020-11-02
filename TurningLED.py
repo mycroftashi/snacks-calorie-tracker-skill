38c8077949febe8844cb282248c757f5ada2760a
mycroftashi <73615955+mycroftashi@users.noreply.github.com>
2020-11-02 14:10:07 -0500

    Update TurningLED.py

--- TurningLED.py
+++ TurningLED.py
@@ -4,13 +4,13 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
 GPIO.setup(23,GPIO.OUT)
-print "LED on Green"
+print ("LED on Green")
 GPIO.output(18,GPIO.HIGH)
 time.sleep(1)
-print "LED off"
+print ("LED off")
 GPIO.output(18,GPIO.LOW)
-print "LED on Orange"
+print ("LED on Orange")
 GPIO.output(23,GPIO.HIGH)
 time.sleep(10)
-print "LED off"
+print ("LED off")
 GPIO.output(23,GPIO.LOW)
