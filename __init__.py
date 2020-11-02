a45e30bbdaa803b630d6e296b5df3ecd5803c999
mycroftashi <73615955+mycroftashi@users.noreply.github.com>
2020-11-02 12:53:50 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,12 +10,12 @@ class SnacksCalorieTracker(MycroftSkill):
     def handle_tracker_calorie_snacks(self, message):
         self.speak_dialog('tracker.calorie.snacks')
         tracker = os.path.expanduser("~/test/DailySnackTracker.json")
-       # filename = os.path.expanduser("~/test/Calorie_Master.json")
-       # with open(filename) as f:
-        #    data = json.load(f)
+        filename = os.path.expanduser("~/test/Calorie_Master.json")
+        with open(filename) as f:
+           data = json.load(f)
 
         # Output: {'name': 'Bob', 'languages': ['English', 'Fench']}
-        #print(data)
+        print(data)
         with open(tracker) as json_file:
             data = json.load(json_file)
 
