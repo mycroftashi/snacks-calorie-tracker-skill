a1418cfb648a9b74765153dd7f47ae50995b2c86
mycroftashi <73615955+mycroftashi@users.noreply.github.com>
2020-11-03 14:50:19 -0500

    ok

--- Read_Write_Calories.py
+++ Read_Write_Calories.py
@@ -1,44 +1,27 @@
 import json
 from datetime import datetime
 
-with open('test/Calorie_Master.json') as f:
+with open('test/Counter.json') as f:
   data = json.load(f)
 
 # Output: {'name': 'Bob', 'languages': ['English', 'Fench']}
 print(data)
 
 
-def write_json(data,temp1, filename='test/DailySnackTracker.json'):
+def write_json(data, filename='test/Counter.json'):
 	print("coming here")
 	with open(filename, 'w') as f:
 			json.dump(data, f, indent=4)
-			json.dump(temp1, f, indent=4)
+
 
 
 
 with open('test/DailySnackTracker.json') as json_file:
 		data = json.load(json_file)
 
-		temp = data['Snacks']
-		today = datetime.today().__str__()
-		# python object to be appended
-		y = {
-							"snack": "Apple",
-	                        "quantity": "1",
-	                         "choice" : "good",
-	                        "consumed": "95",
-	                       "date and time": today
-		     }
-		temp.append(y)
+		data['counter_healthy'] = "10"
 
 
 
-		for data_set in data.get("Counter", {}):
-			_extract = data_set.get("count_unhealthy", None)
-			print (_extract + "thsi si teh value ")
-		#print(temp1['count_unhealthy'] +"Asha")
-		#temp1["count_unhealthy"] = int(temp1["count_unhealthy"]) + 1
-		#temp1["date and time"] = today
 
-
-write_json(data,_extract)
+write_json(data)
