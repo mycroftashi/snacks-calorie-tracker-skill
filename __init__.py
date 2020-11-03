5d55638bc7454f4604e58a48294951305e8cce8c
mycroftashi <73615955+mycroftashi@users.noreply.github.com>
2020-11-02 21:04:20 -0500

    a

--- __init__.py
+++ __init__.py
@@ -3,15 +3,24 @@ from mycroft import MycroftSkill, intent_handler
 
 class SnacksCalorieTracker(MycroftSkill):
 
+    @intent_handler(IntentBuilder('SnackingIntent')
+                    .require('SnackKeyword'))
+    def handle_snacking_intent(self, message):
+        self.speak_dialog("WarnCalorie")
+
+
     @intent_handler(IntentBuilder('DeclineAdviceIntent').require('DeclineKeyword'))
     def handle_decline_intent(self, message):
         """ This is an Adapt intent handler, it is triggered by a keyword."""
         self.speak_dialog("TrackSnacksAdvice")
 
-    @intent_handler(IntentBuilder('SnackingIntent')
-                    .require('SnackKeyword'))
-    def handle_snacking_intent(self, message):
-        self.speak_dialog("WarnCalorie")
+
+
+    @intent_handler(IntentBuilder('ListeningIntent')
+                    .require('ListenKeyword'))
+    def handle_listen_to_advice_intent(self, message):
+        self.speak_dialog("WellDoneMessage")
+
 
     def stop(self):
         pass
