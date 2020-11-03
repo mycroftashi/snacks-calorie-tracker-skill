abe009f354f2d55134641bf2a990c60bb11be4bb
mycroftashi <73615955+mycroftashi@users.noreply.github.com>
2020-11-02 19:33:02 -0500

    g

--- __init__.py
+++ __init__.py
@@ -3,15 +3,9 @@ from mycroft import MycroftSkill, intent_handler
 
 class SnacksCalorieTracker(MycroftSkill):
 
-    @intent_handler(IntentBuilder('ThankYouIntent').require('ThankYouKeyword'))
-    def handle_thank_you_intent(self, message):
+    @intent_handler(IntentBuilder('DeclineAdviceIntent').require('DeclineKeyword'))
+    def handle_decline_intent(self, message):
         """ This is an Adapt intent handler, it is triggered by a keyword."""
-        self.speak_dialog("welcome")
-
-    @intent_handler('DeclineAdviceIntent')
-    def handle_decline_advice(self, message):
-        """ This is a Padatious intent handler.
-		It is triggered using a list of sample phrases."""
         self.speak_dialog("TrackSnacksAdvice")
 
     @intent_handler(IntentBuilder('SnackingIntent')
