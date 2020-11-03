d738eef9f6f3bdd54474923e7a3c0acea944612d
mycroftashi <73615955+mycroftashi@users.noreply.github.com>
2020-11-02 19:04:19 -0500

    add

--- __init__.py
+++ __init__.py
@@ -14,8 +14,8 @@ class SnacksCalorieTracker(MycroftSkill):
 		It is triggered using a list of sample phrases."""
         self.speak_dialog("how.are.you")
 
-    @intent_handler(IntentBuilder('HelloWorldIntent')
-                    .require('HelloWorldKeyword'))
+    @intent_handler(IntentBuilder('SnackingIntent')
+                    .require('SnackKeyword'))
     def handle_hello_world_intent(self, message):
         """ Skills can log useful information. These will appear in the CLI and
 		the skills.log file."""
