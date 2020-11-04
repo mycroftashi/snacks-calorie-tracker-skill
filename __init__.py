63811aa20906bb36ac51d9c00cccb2d83f7b7a3d
mycroftashi <73615955+mycroftashi@users.noreply.github.com>
2020-11-04 17:17:14 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -71,7 +71,7 @@ class SnacksCalorieTracker(MycroftSkill):
                             #Update unhealthy counter
                             if int(current_unhealthy_counter) >= 4:
                                 # the following line needs your Twilio Account SID and Auth Token
-                                client = Client("AC429a4c06f04eb36287f1c2a682c90a2a", "2d7e4dc79393ccbbd8d4827d076fa24c")
+                                client = Client("AC429a4c06f04eb36287f1c2a682c90a2a", "1b9a727dfcfe6800beced5f764a3e9be")
                                 # change the "from_" number to your Twilio number and the "to" number
                                 # to the phone number you signed up for Twilio with, or upgrade your
                                 # account to send SMS to any phone number
