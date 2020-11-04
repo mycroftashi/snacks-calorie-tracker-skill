065a9c7b9081d2d3da1d9eb00ad5dd43d6d5969c
mycroftashi <73615955+mycroftashi@users.noreply.github.com>
2020-11-04 12:44:15 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -47,7 +47,6 @@ class SnacksCalorieTracker(MycroftSkill):
                         choice = data_set.get("choice", None)
                         info = data_set.get("Info", None)
                         calorie = data_set.get("Calorie", None)
-
                         # if snack choice is unhealthy
                         if choice == "bad":
                             print("Signal unhealthy snack")
@@ -55,71 +54,58 @@ class SnacksCalorieTracker(MycroftSkill):
                             GPIO.output(23, GPIO.HIGH)
                             #Give more info about unhealthy snack to reconsider choice
                             self.speak("Ok Avyan " + info + calorie + " bad calories and sugar in it, which will make you restless .")
-
                             # Wait for response to see if choice is changed
                             reconsider_choice = self.ask_yesno("WarnCalorie")
-
-
                             if reconsider_choice == 'yes':
-                                self.speak("Ok, not a great choice, but let me make an entry")
-
-                                #switch off LED
-                                GPIO.output(23, GPIO.HIGH)
-                                # python object to be appended
-
-                                # Update unhealthy counter by 1
-                                with open(counter) as json_file:
-                                    data = json.load(json_file)
-                                    data['counter_unhealthy'] = int(data['counter_unhealthy']) + int("1")
-                                    current_unhealthy_counter = str(data['counter_unhealthy'])
-                                    #Update unhealthy counter
+                                    self.speak("Ok, not a great choice, but let me make an entry")
+                                    #switch off LED
+                                    GPIO.output(23, GPIO.HIGH)
+                                    # python object to be appended
+                                    # Update unhealthy counter by 1
+                                    with open(counter) as json_file:
+                                        data = json.load(json_file)
+                                        data['counter_unhealthy'] = int(data['counter_unhealthy']) + int("1")
+                                        current_unhealthy_counter = str(data['counter_unhealthy'])
+                                        #Update unhealthy counter
                                     with open(counter, 'w') as counterf:
-                                        json.dump(data, counterf, indent=4)
-                                        print("Increased the unhealthy counter by 1 ")
-                                        self.speak("Today you have eaten " + current_unhealthy_counter + "unhealthy snacks" )
+                                            json.dump(data, counterf, indent=4)
+                                            print("Increased the unhealthy counter by 1 ")
+                                            self.speak("Today you have eaten " + current_unhealthy_counter + "unhealthy snacks" )
+                                            #Send SMS if unhealthy snacks is meeting teh threshhold limit
+                                    if int(current_unhealthy_counter) >= 4:
+                                                # the following line needs your Twilio Account SID and Auth Token
+                                                client = Client("AC429a4c06f04eb36287f1c2a682c90a2a", "2d7e4dc79393ccbbd8d4827d076fa24c")
 
+                                                # change the "from_" number to your Twilio number and the "to" number
+                                                # to the phone number you signed up for Twilio with, or upgrade your
+                                                # account to send SMS to any phone number
+                                                client.messages.create(to="+12012400693", from_ = "+16267095806", body = "Alert - Avyan is eating way too much unhealthy snack today!")
+                                                # start - finish sms
 
-                                        #Send SMS if unhealthy snacks is meeting teh threshhold limit
-                                        if int(current_unhealthy_counter) >= 4:
-                                            # the following line needs your Twilio Account SID and Auth Token
-                                            client = Client("AC429a4c06f04eb36287f1c2a682c90a2a", "2d7e4dc79393ccbbd8d4827d076fa24c")
+                                    # Add entry in Daily snack tracker for unhealthy snack intent
+                                    with open(tracker) as tracker_file:
+                                        dataw = json.load(tracker_file)
+                                        item = dataw['Snacks']
+                                        today = datetime.today().__str__()
+                                        y = {
+                                            "snack": _extract.upper(),
+                                            "quantity": "1",
+                                            "choice": "bad",
+                                            "consumed": calorie,
+                                            "date and time": today
 
-                                            # change the "from_" number to your Twilio number and the "to" number
-                                            # to the phone number you signed up for Twilio with, or upgrade your
-                                            # account to send SMS to any phone number
-                                            client.messages.create(to="+12012400693", from_ = "+16267095806", body = "Alert - Avyan is eating way too much unhealthy snack today!")
-                                            # start - finish sms
-
-                                # Add entry in Daily snack tracker for unhealthy snack intent
-                                with open(tracker) as tracker_file:
-                                    dataw = json.load(tracker_file)
-                                    item = dataw['Snacks']
-                                    today = datetime.today().__str__()
-                                    y = {
-                                        "snack": _extract.upper(),
-                                        "quantity": "1",
-                                        "choice": "bad",
-                                        "consumed": calorie,
-                                        "date and time": today
-
-                                    }
-                                    item.append(y)
-
-                                with open(tracker, 'w') as trackerf:
-                                    json.dump(dataw, trackerf, indent=4)
-                                    self.speak ("All done")
-
-                                    print("Made an unhealthy food entry for " +_extract + "in the tracker" )
-
+                                        }
+                                        item.append(y)
+                                    with open(tracker, 'w') as trackerf:
+                                        json.dump(dataw, trackerf, indent=4)
+                                        self.speak ("All done")
+                                        print("Made an unhealthy food entry for " +_extract + "in the tracker" )
 
                             if reconsider_choice == 'no':
-
-                                GPIO.output(23, GPIO.LOW)
-                                GPIO.output(18, GPIO.HIGH)
-                                wavfile = os.path.expanduser("~/hello.wav")
-                                play_wav(wavfile)
-
-
+                                    GPIO.output(23, GPIO.LOW)
+                                    GPIO.output(18, GPIO.HIGH)
+                                    wavfile = os.path.expanduser("~/hello.wav")
+                                    play_wav(wavfile)
 
                         # if snack choice is healthy
                         if choice == "good":
