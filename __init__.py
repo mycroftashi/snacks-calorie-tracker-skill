b56891da59fa861304440bffd5dd4a8d97492c3d
mycroftashi <73615955+mycroftashi@users.noreply.github.com>
2020-11-04 14:27:22 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -36,103 +36,104 @@ class SnacksCalorieTracker(MycroftSkill):
         with open(filename) as f:
             data = json.load(f)
 
-        for data_set in data.get("Items", {}):
+            for data_set in data.get("Items", {}):
                 _extract = data_set.get("name", None)
                 print("executing for loop")
                 choice = ""
                 info =""
                 calorie=""
+                print("choice" + choice)
 
                 if _extract.upper() in usr_message.upper():
                  # find out details about the snack
-                     choice = data_set.get("choice", None)
-                     info = data_set.get("Info", None)
-                     calorie = data_set.get("Calorie", None)
+                    choice = data_set.get("choice", None)
+                    info = data_set.get("Info", None)
+                    calorie = data_set.get("Calorie", None)
+                    print("choice" + choice)
 
-                print(choice)
 
-                # if snack choice is unhealthy
-                if choice == "bad":
-                    print("Signal unhealthy snack")
-                    #Lit Red LED
-                    GPIO.output(23, GPIO.HIGH)
-                    #Give more info about unhealthy snack to reconsider choice
-                    self.speak("Ok Avyan " + info + calorie + " bad calories and sugar in it, which will make you restless .")
-                    # Wait for response to see if choice is changed
-                    #reconsider_choice = self.ask_yesno("WarnCalorie")
-                    # Update unhealthy counter by 1
-                    with open(counter) as json_file:
-                       data = json.load(json_file)
-                       current_unhealthy_counter = str(data['counter_unhealthy'])
-                    #Update unhealthy counter
-                    if int(current_unhealthy_counter) >= 4:
-                        # the following line needs your Twilio Account SID and Auth Token
-                        client = Client("AC429a4c06f04eb36287f1c2a682c90a2a", "2d7e4dc79393ccbbd8d4827d076fa24c")
-                        # change the "from_" number to your Twilio number and the "to" number
-                        # to the phone number you signed up for Twilio with, or upgrade your
-                        # account to send SMS to any phone number
-                        client.messages.create(to="+12012400693", from_ = "+16267095806", body = "Alert - Avyan is eating way too much unhealthy snack today!")
-                        # start - finish sms
+                    # if snack choice is unhealthy
+                    if choice == "bad":
+                            print("Signal unhealthy snack")
+                            #Lit Red LED
+                            GPIO.output(23, GPIO.HIGH)
+                            #Give more info about unhealthy snack to reconsider choice
+                            self.speak("Ok Avyan " + info + calorie + " bad calories and sugar in it, which will make you restless .")
+                            # Wait for response to see if choice is changed
+                            #reconsider_choice = self.ask_yesno("WarnCalorie")
+                            # Update unhealthy counter by 1
+                            with open(counter) as json_file:
+                               data = json.load(json_file)
+                               current_unhealthy_counter = str(data['counter_unhealthy'])
+                            #Update unhealthy counter
+                            if int(current_unhealthy_counter) >= 4:
+                                # the following line needs your Twilio Account SID and Auth Token
+                                client = Client("AC429a4c06f04eb36287f1c2a682c90a2a", "2d7e4dc79393ccbbd8d4827d076fa24c")
+                                # change the "from_" number to your Twilio number and the "to" number
+                                # to the phone number you signed up for Twilio with, or upgrade your
+                                # account to send SMS to any phone number
+                                client.messages.create(to="+12012400693", from_ = "+16267095806", body = "Alert - Avyan is eating way too much unhealthy snack today!")
+                                # start - finish sms
 
-                    # Add entry in Daily snack tracker for unhealthy snack intent
-                    with open(tracker) as tracker_file:
-                       dataw = json.load(tracker_file)
-                       item = dataw['Snacks']
-                       today = datetime.today().__str__()
+                            # Add entry in Daily snack tracker for unhealthy snack intent
+                            with open(tracker) as tracker_file:
+                               dataw = json.load(tracker_file)
+                               item = dataw['Snacks']
+                               today = datetime.today().__str__()
 
-                       y = {
-                                            "snack": _extract.upper(),
-                                            "quantity": "1",
-                                            "choice": "bad",
-                                            "consumed": calorie,
-                                            "date and time": today
+                               y = {
+                                                    "snack": _extract.upper(),
+                                                    "quantity": "1",
+                                                    "choice": "bad",
+                                                    "consumed": calorie,
+                                                    "date and time": today
 
-                            }
-                       item.append(y)
+                                    }
+                               item.append(y)
 
-                    with open(tracker, 'w') as trackerf:
-                            json.dump(dataw, trackerf, indent=4)
-                            self.speak ("All done")
-                            print("Made an unhealthy food entry for " +_extract + "in the tracker" )
+                            with open(tracker, 'w') as trackerf:
+                                    json.dump(dataw, trackerf, indent=4)
+                                    self.speak ("All done")
+                                    print("Made an unhealthy food entry for " +_extract + " in the tracker" )
 
-                # if snack choice is healthy
-                if choice == "good":
-                     #Lit the green signal for healthy snacking
-                    print("Lit the green signal and switch off red signal for healthy snacking")
-                    GPIO.output(23, GPIO.LOW)
-                    GPIO.output(18, GPIO.HIGH)
-                    #Encourage good choice
-                    self.speak("Ok Avyan " + info + calorie + " good calories in it, so you have made a healthy choice, Let me mark it")
-                    #Update counter of healthy snacking
-                    with open(counter) as json_file:
-                        data = json.load(json_file)
-                        data['counter_healthy'] = int(data['counter_healthy']) + int("1")
+                        # if snack choice is healthy
+                    if choice == "good":
+                             #Lit the green signal for healthy snacking
+                            print("Lit the green signal and switch off red signal for healthy snacking")
+                            GPIO.output(23, GPIO.LOW)
+                            GPIO.output(18, GPIO.HIGH)
+                            #Encourage good choice
+                            self.speak("Ok Avyan " + info + calorie + " good calories in it, so you have made a healthy choice, Let me mark it")
+                            #Update counter of healthy snacking
+                            with open(counter) as json_file:
+                                data = json.load(json_file)
+                                data['counter_healthy'] = int(data['counter_healthy']) + int("1")
 
-                    with open(counter, 'w') as cf:
-                        json.dump(data, cf, indent=4)
-                        print("Increased the healthy counter by 1 ")
+                            with open(counter, 'w') as cf:
+                                json.dump(data, cf, indent=4)
+                                print("Increased the healthy counter by 1 ")
 
-                    # Add new entry for  healthy snacking
-                    with open(tracker) as tracker_file:
-                        dataw = json.load(tracker_file)
-                        item = dataw['Snacks']
-                        today = datetime.today().__str__()
-                        y = {
-                                            "snack": _extract.upper(),
-                                            "quantity": "1",
-                                            "choice": "good",
-                                            "consumed": calorie,
-                                            "date and time": today
+                            # Add new entry for  healthy snacking
+                            with open(tracker) as tracker_file:
+                                dataw = json.load(tracker_file)
+                                item = dataw['Snacks']
+                                today = datetime.today().__str__()
+                                y = {
+                                                    "snack": _extract.upper(),
+                                                    "quantity": "1",
+                                                    "choice": "good",
+                                                    "consumed": calorie,
+                                                    "date and time": today
 
-                            }
-                        item.append(y)
+                                    }
+                                item.append(y)
 
-                    with open(tracker, 'w') as tf:
-                        json.dump(dataw, tf, indent=4)
-                        self.speak("All done")
-                        print("Made an healthy food entry for " + _extract + "in the tracker")
+                            with open(tracker, 'w') as tf:
+                                json.dump(dataw, tf, indent=4)
+                                self.speak("All done")
+                                print("Made an healthy food entry for " + _extract + "in the tracker")
 
-                break
+                            break
 
     @intent_handler(IntentBuilder('DeclineAdviceIntent').require('DeclineKeyword'))
     def handle_decline_intent(self, message):
